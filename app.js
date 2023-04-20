2d9deacd30411748bbcd89318ad4cbae29794cee
Furkan <furkan18388@gmail.com>
2023-04-20 21:03:00 +0300

    linkler güncellendi

--- app.js
+++ app.js
@@ -2,8 +2,23 @@ const express = require("express");
 
 const app = express();
 
+// Template Engine
+app.set("view engine", "ejs");
+
+// Middleware
+app.use(express.static("public"));
+
+// Routes
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.status(404).render("index", {
+    page_name: "index",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).render("about", {
+    page_name: "about",
+  });
 });
 
 const port = 3000;
